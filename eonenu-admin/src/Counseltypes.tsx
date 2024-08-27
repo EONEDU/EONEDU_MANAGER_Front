7fe230a9590f8dec32e1480a3a8b84e2d2930b83
ohhamma <judyham10634@naver.com>
2024-08-28 04:09:23 +0900

    feat: show counsel type

--- eonenu-admin/src/Counseltypes.tsx
+++ eonenu-admin/src/Counseltypes.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { 
     List,
     Create,
+    Show,
     SimpleForm,
+    SimpleShowLayout,
     TextInput,
     Datagrid,
     TextField
@@ -10,7 +12,7 @@ import {
 
 export const CounselTypeList = (props: any) => (
     <List {...props}>
-        <Datagrid rowClick="edit">
+        <Datagrid rowClick="show">
             <TextField source="id" label="상담유형 ID" />
             <TextField source="name" label="이름" />
         </Datagrid>
@@ -23,4 +25,13 @@ export const CounselTypeCreate = (props: any) => (
             <TextInput source="name" label="상담유형 이름" />
         </SimpleForm>
     </Create>
+);
+
+export const CounselTypeShow = (props: any) => (
+    <Show {...props}>
+        <SimpleShowLayout>
+            <TextField source="id" label="상담유형 ID" />
+            <TextField source="name" label="이름" />
+        </SimpleShowLayout>
+    </Show>
 );
